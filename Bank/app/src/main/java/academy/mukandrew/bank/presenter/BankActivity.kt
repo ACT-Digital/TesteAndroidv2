e17c50985378e93bfed5c1031365bc5c34f36b6b
Murillo <murilloandrew@gmail.com>
2020-06-13 19:36:03 -0300

    - Setup project structures and libs

---
+++ Bank/app/src/main/java/academy/mukandrew/bank/presenter/BankActivity.kt
@@ -0,0 +1,12 @@
+package academy.mukandrew.bank.presenter
+
+import academy.mukandrew.bank.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BankActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
