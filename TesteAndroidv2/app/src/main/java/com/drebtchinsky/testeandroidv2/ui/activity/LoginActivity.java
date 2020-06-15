21b84506cc4a70cdfc4c6c553f06e9c263655914
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2020-06-15 14:50:31 -0300

    layout

---
+++ TesteAndroidv2/app/src/main/java/com/drebtchinsky/testeandroidv2/ui/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.drebtchinsky.testeandroidv2.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.drebtchinsky.testeandroidv2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
